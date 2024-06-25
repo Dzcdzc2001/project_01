32de4c35f73d151958007394b065785d0349e271
DZCdzc2001 <109464651+Dzcdzc2001@users.noreply.github.com>
2024-06-25 11:29:23 +0800

    iterative record

--- code/Node_JS/目录操作/CRUD.js
+++ code/Node_JS/目录操作/CRUD.js
@@ -1,6 +1,18 @@
 const fs = require("fs");
 
+// 创建文件夹
+// fs.mkdirSync("111");
 
+//删除文件夹 只能删除空文件夹
+// fs.rmdirSync("111");
+
+//重命名
+// fs.renameSync("111","222");
+
+//读取文件夹 只能读取一层
+// fs.readdirSync("111");
+
+//利用递归实现完全删除文件夹
 function myRemove(src) {
     try {
         let arr = fs.readdirSync(src);
@@ -8,16 +20,17 @@ function myRemove(src) {
             fs.rmdirSync(src);
         } else {
             arr.forEach(item => {
-                if (fs.statSync(item).isDirectory()) {
-                    myRemove(item);
+                if (fs.statSync(src+"/"+item).isDirectory()) {
+                    myRemove(src+"/"+item);
                 } else {
-                    fs.unlinkSync(item);
+                    fs.unlinkSync(src+"/"+item);
                 }
             })
+            fs.rmdirSync(src);
         }
     } catch (err) {
         console.log(err);
     }
 
 }
-myRemove("111");
+// myRemove("./111");
